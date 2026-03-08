c81fc8c91ab1337f73424752cfc46875813f30c8
Sujitha <sujitha899s@gmail.com>
2026-03-08 14:17:15 +0530

    Solved Gcd by Euclidean algorithm

--- BasicMath/GCD.java
+++ BasicMath/GCD.java
@@ -5,6 +5,25 @@ public class GCD {
         Scanner sc= new Scanner(System.in);
         System.out.println("enter the number: ");
         int n= sc.nextInt();
+        int m=sc.nextInt();
+        System.out.println("gcd is: "+gcd(n,m));
         sc.close();
     }
+
+        // if we solve it with for loop, (brute force) then it has complexity of O(n)
+        // so we will use euclidean algorithm
+
+        //gcd(a,b)=gcd(a%b,b) given than a>b
+    static int gcd(int a,int b){
+
+        while(a>0&& b>0){
+            if(a>b) a=a%b;
+            else b=b%a;
+
+        }
+        if(a==0) return b;
+        return a;
+    }
+        
+    
 }
--- BasicMath/checkPalindrome.java
+++ BasicMath/checkPalindrome.java
@@ -6,7 +6,9 @@ public class checkPalindrome {
             for(int i=0;a>0;i++){
                 b=b*10+ (a%10);
                 a=a/10;
+                
             }
+            
             return b;
         }
     public static void main(String args[]){
