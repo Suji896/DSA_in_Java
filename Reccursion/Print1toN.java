b696016a6622a023d44afbbfa1fec4dc7110e8c7
Sujitha <sujitha899s@gmail.com>
2026-03-09 23:50:12 +0530

    Recursion: print 1 to N

---
+++ Reccursion/Print1toN.java
@@ -0,0 +1,30 @@
+package Reccursion;
+
+import java.util.Scanner;
+
+public class Print1toN {
+    static void print1toN (int a){
+        
+        if(a==0){
+            return;
+        }
+       
+        
+        print1toN(a-1);
+        System.out.println(a);
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n= sc.nextInt();
+        
+        System.out.println("output:");
+        print1toN(n);
+        
+        sc.close();
+
+    }
+}
+
+    
+
