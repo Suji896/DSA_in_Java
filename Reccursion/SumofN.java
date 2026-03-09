a9eaed77b7847b80c5826702cf103207aedc9615
Sujitha <sujitha899s@gmail.com>
2026-03-09 23:56:18 +0530

    Reccursion: Sum of N numbers

---
+++ Reccursion/SumofN.java
@@ -0,0 +1,20 @@
+package Reccursion;
+import java.util.Scanner;
+public class SumofN {
+    static int sum(int n){
+        if(n==0){
+            return 0;
+        }
+        return n+ sum(n-1);
+
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number:");
+        int a= sc.nextInt();
+        System.out.println("sum of n numbers:");
+        System.out.println(sum(a));
+
+        sc.close();
+    }
+}
