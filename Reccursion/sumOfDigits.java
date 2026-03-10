42381fb53e53b6b62cd06caccfe29cbb616fba55
Sujitha <sujitha899s@gmail.com>
2026-03-10 20:44:02 +0530

    Reccursion: Sum of Digits of a number

---
+++ Reccursion/sumOfDigits.java
@@ -0,0 +1,21 @@
+package Reccursion;
+import java.util.Scanner;
+public class sumOfDigits {
+    static int DigitSum(int a){
+        if(a==0){
+            return 0;
+        }
+
+        return (a%10)+DigitSum((a/10));
+
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n= sc.nextInt();
+        System.out.print("Sum of Digits: ");
+        System.out.println(DigitSum(n));
+        sc.close();
+    }
+    
+}
