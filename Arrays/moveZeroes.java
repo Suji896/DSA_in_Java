641baac327760914a2507850343110b4013e6bb2
Sujitha <sujitha899s@gmail.com>
2026-03-29 19:06:09 +0530

    Arrays: Move Zeroes to end

--- Arrays/moveZeroes.java
+++ Arrays/moveZeroes.java
@@ -5,16 +5,13 @@ public class moveZeroes {
         // int arr[]={0,1,0,3,4};
         int arr[]={1,0,4,5,0,6,0,7};
         int n= arr.length;
-        
-        for(int i=0;i<n-1;i++){  
-            if(arr[i]==0){
-                for(int j=i;j<=n-1;j++){
-                    if(arr[j]!=0){
-                        arr[i]=arr[j];
-                        arr[j]=0;
-                        break;
-                    }
-                }
+        int i=0;
+        for(int j=0;j<n;j++){
+            if(arr[j]!=0){
+                int temp=arr[j];
+                arr[j]=arr[i];
+                arr[i]=temp;
+                i++;
             }
         }
         
