76ef19eac3c1a568308010f8398edbc63d9109b8
Sujitha <sujitha899s@gmail.com>
2026-03-30 14:55:03 +0530

    Arrays: Solved Single Number Problem

--- Arrays/singleNumber.java
+++ Arrays/singleNumber.java
@@ -4,6 +4,13 @@ public class singleNumber {
     public static void main(String[] args) {
         int [] arr={1,2,3,2,4,1,3};
         int n= arr.length;
-        
+        int s=0;
+        for(int i=0;i<n;i++){
+            s=s^arr[i];
+
+        }
+        System.out.println("Single number found: "+ s);
+       
+       
     }
 }
